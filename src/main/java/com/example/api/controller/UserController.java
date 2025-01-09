4c74bf11faa2dfe689dba5c154b05ea4604cdfb7
amey.mo <amey.mo@media.net>
2025-01-09 15:06:02 +0530

    remove controller methods

--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -32,48 +32,4 @@ public class UserController {
     public ResponseEntity<Author[]> getAllAuthors() {
         return ResponseEntity.ok(authorService.getAllAuthors());
     }
-
-    // Add a new author
-    @PostMapping(path = "/authors", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Author[]> addAuthor(@RequestBody Author author) {
-        return ResponseEntity.ok(authorService.addAuthor(author));
-    }
-
-    // Update an existing author
-    @PutMapping(path = "/authors", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Author> updateAuthor(@RequestBody Author author) {
-        return ResponseEntity.ok(authorService.updateAuthor(author));
-    }
-
-    // Delete an author by ID
-    @DeleteMapping(path = "/authors/{id}")
-    public ResponseEntity<Author[]> deleteAuthor(@PathVariable int id) {
-        return ResponseEntity.ok(authorService.deleteAuthorById(id));
-    }
-
-    // ---- BOOK ENDPOINTS ----
-
-    // Fetch all books
-    @GetMapping(path = "/books")
-    public ResponseEntity<List<Book>> getAllBooks() {
-        return ResponseEntity.ok(bookService.getAllBooks());
-    }
-
-    // Add a new book
-    @PostMapping(path = "/books", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Book> addBook(@RequestBody Book book) {
-        return ResponseEntity.ok(bookService.addBook(book));
-    }
-
-    // Update a book by ID
-    @PutMapping(path = "/books/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Book> updateBook(@PathVariable int id, @RequestBody Book updatedBook) {
-        return ResponseEntity.ok(bookService.updateBook(id, updatedBook));
-    }
-
-    // Delete a book by ID
-    @DeleteMapping(path = "/books/{id}")
-    public ResponseEntity<List<Book>> deleteBook(@PathVariable int id) {
-        return ResponseEntity.ok(bookService.deleteBookById(id));
-    }
 }
